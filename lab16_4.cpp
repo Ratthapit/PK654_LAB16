446836b4a1981376897a4c072094464d7c8cc936
Ratthapit <pimpitchakanitpanyajaroen@gmail.com>
2022-01-17 23:40:04 +0700

    lab16_4.cpp completed

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,35 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d){
+    int Shuf[4],count=0;
+    for(int i=0;i<4;i++){
+    	Shuf[i]= rand()%4+1;
+    	if(i!=0&&Shuf[0]==Shuf[i])	i--;
+    	if(i!=1&&Shuf[1]==Shuf[i])	i--;
+    	if(i!=2&&Shuf[2]==Shuf[i])	i--;
+	}
+	
+	for(int i=0;i<4;i++){
+		if(Shuf[i]==1)		Shuf[i]=50;
+		else if(Shuf[i]==2)	Shuf[i]=100;
+		else if(Shuf[i]==3)	Shuf[i]=500;
+		else if(Shuf[i]==4)	Shuf[i]=1000;
+	}
+	
+	if(Shuf[0]==a)	count++;
+	if(Shuf[1]==b)	count++;
+	if(Shuf[2]==c)	count++;
+	if(Shuf[3]==d)	count++;
+
+    if(count==4)	shuffle(Shuf[0],Shuf[1],Shuf[3],Shuf[4]);
+    
+    if(Shuf[0]!=Shuf[1]&&Shuf[0]!=Shuf[2]&&Shuf[0]!=Shuf[3]&&Shuf[1]!=Shuf[2]&&Shuf[1]!=Shuf[3]&&Shuf[2]!=Shuf[3]){
+        a=Shuf[0];
+	    b=Shuf[1];
+	    c=Shuf[2];
+	    d=Shuf[3];
+	}
+	else    shuffle(Shuf[0],Shuf[1],Shuf[3],Shuf[4]);
+}
