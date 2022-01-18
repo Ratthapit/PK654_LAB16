5721c63e6daceebd9b94f36de0d2314da8fe3083
Ratthapit <pimpitchakanitpanyajaroen@gmail.com>
2022-01-18 13:11:08 +0700

    lab16_1.cpp completed

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,28 @@
-include <iostream>
+#include<iostream>
 using namespace std;
 
 int main(){
-
+	int a=5;
+    char b=65;
+    char &c=b;
+    int *x=&a;
+    void *y=&b;
+    void *z=&x;
+    cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<" \n";
+        
+	cout<<&a<<" "<<(void *) &b<<" "<<(void *) &c<<" "<<&x<<" "<<&y<<" "<<&z<<" \n";
 	
+	*(&c)=70;
+	cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<" \n";
+	
+	*(char*)y =87;
+	cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<" \n";
+	
+	*x=6;
+	cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<" \n";
+	
+	**(int **)z=7;
+	cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<" \n";
 	
 	return 0;
 }
